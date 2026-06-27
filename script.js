a31f95b2256007e0a1c8ce3657af6ea74ef10cc3
gemasaurav <gema.saurav@gmail.com>
2026-06-27 20:30:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
+function convertText() {
 
+    const input = document
+        .getElementById("inputText")
+        .value
+        .toUpperCase();
+
+    let output = "";
+
+    for(let i=0;i<input.length;i++){
+
+        let ch = input[i];
+
+        if(symbolMap[ch]){
+
+            output +=
+            `<img src="${symbolMap[ch]}" class="symbol">`;
+
+        }
+        else if(ch===" "){
+
+            output += "&nbsp;&nbsp;&nbsp;&nbsp;";
+
+        }
+        else{
+
+            output += ch;
+
+        }
+
+    }
+
+    document.getElementById("output").innerHTML = output;
+
+}
+
+function clearText(){
+
+    document.getElementById("inputText").value="";
+
+    document.getElementById("output").innerHTML="";
+
+}
