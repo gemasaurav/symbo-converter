44fc77ffb8f7b7808ed46a27c669543f70a272e1
gemasaurav <gema.saurav@gmail.com>
2026-07-02 09:52:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -221,3 +221,71 @@ convertBtn.addEventListener("click",function(){
     }
 
 });
+// ======================================================
+// SYMBO CONVERTER V1.0
+// PART 3
+// ======================================================
+
+// =====================================
+// Copy Button
+// =====================================
+
+copyBtn.addEventListener("click", async function(){
+
+    if(hiddenOutput.trim()===""){
+
+        alert("Nothing to copy.");
+
+        return;
+
+    }
+
+    try{
+
+        await navigator.clipboard.writeText(hiddenOutput);
+
+        alert("Copied Successfully!");
+
+    }
+
+    catch(err){
+
+        const temp=document.createElement("textarea");
+
+        temp.value=hiddenOutput;
+
+        document.body.appendChild(temp);
+
+        temp.select();
+
+        document.execCommand("copy");
+
+        document.body.removeChild(temp);
+
+        alert("Copied Successfully!");
+
+    }
+
+});
+
+// =====================================
+// Clear Button
+// =====================================
+
+clearBtn.addEventListener("click",function(){
+
+    inputBox.value="";
+
+    outputArea.innerHTML="Converted output will appear here...";
+
+    hiddenOutput="";
+
+    inputBox.focus();
+
+});
+
+// =====================================
+// Initial Screen
+// =====================================
+
+outputArea.innerHTML="Converted output will appear here...";
